fadcf64d5d6636085d30b93fb67e9aac6d903bdb
MichaelBoyo <103725264+MichaelBoyo@users.noreply.github.com>
2022-06-04 10:17:52 +0100

    estore

---
+++ EStore/BillingInformation.java
@@ -0,0 +1,12 @@
+package EStore;
+
+
+import java.util.ArrayList;
+
+public class BillingInformation {
+    private String receiversPhoneNumber;
+    private String receiversName;
+    Address address;
+
+    CreditCardInformation cardInfo;
+}
---
+++ EStore/Customers.java
@@ -0,0 +1,9 @@
+package EStore;
+
+import java.util.ArrayList;
+
+public class Customers extends Users {
+
+    BillingInformation billingInformation;
+    ShoppingCart shoppingCarts;
+}
